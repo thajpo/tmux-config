ae54c764a64d2e31e59df41b666bfd0b52cc0f25
Jason Pettinato <66739444+thajpo@users.noreply.github.com>
2026-02-08 17:59:35 -0500

    chore(dotfiles): automated sync 2026-02-08T22:59:35Z

---
+++ nvim/lua/plugins/surround.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "gsa",
+        delete = "gsd",
+        replace = "gsr",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        update_n_lines = "gsn",
+      },
+    },
+  },
+}
